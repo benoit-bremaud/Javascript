3e8e39ac219364ac9c0943e5909e8e8a65eed7f1
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-12 15:00:01 +0200

    feat(variables): Declare, assign values, and log to console
    This commit introduces variable declarations, assigns values, and logs them to the console for debugging or informational purposes.
    
    The code declares the following variables:
    - `nombre` with an initial value of 3, later assigned a value of 25.
    - `autreNombre` with an initial value of 2, later assigned a value of 15.36.
    - `texte` with an initial value of "Bonjour".
    
    The code then uses `console.log()` to output the following:
    - "Brouette" is logged to the console.
    - The value of the `nombre` variable is logged to the console.
    - The value of the `texte` variable is logged to the console.
    
    Please note that this code snippet is primarily used for demonstration purposes and debugging, and should be removed or modified as needed in production.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@
 // Poser une question
 // prompt("Quel nombre ?");
 
-confirm("Etes vous sûr(e) ?");
+// confirm("Etes vous sûr(e) ?");
 
+// Utilisation de variable
+// On crée la variable nombre et on lui affecte la valeur 3
+let nombre = 3;
+
+var autreNombre = 2;
+
+nombre = 25;
+
+let texte = "Bonjour"; // Variable texte = chaîne de caractère
+
+autreNombre = 15.36; // La variable autreNombre est de type : nombre
+
+// Ecrire dans la console
+console.log("Brouette");    // Affiche "Brouette" dans la console
+console.log(nombre);        // Affiche le contenu de la variable nombre dans la console
+console.log(texte);         
 
