5025e5a15baeabf88eefd7d5b81da2e0011bce38
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-13 08:37:41 +0200

    feat(script): Implement basic arithmetic operations and console logging
    Implemented basic arithmetic operations (+, -, *, /) and demonstrated their usage in script.js. Also added console.log statement to display the value of the variable 'nombre'.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,45 @@
-// Intéraction avec l'utilisateur
-// Affiche un message d'alert à l'utilisateur
-// alert("Message");
+let nombre1 = 12;
+let nombre2 = 25;
 
-// Poser une question
-// prompt("Quel nombre ?");
+// Opérations de base +, -, *, /
+let nombre = nombre1 + nombre2;
 
-// confirm("Etes vous sûr(e) ?");
+nombre = nombre1 - nombre2;
 
-// Utilisation de variable
-// On crée la variable nombre et on lui affecte la valeur 3
-let nombre = 3;
+nombre = nombre1 * nombre2;
 
-var autreNombre = 2;
+nombre = nombre1 / nombre2;
 
-nombre = 25;
+// Priorité des opérations
+nombre = 2+3*5;
 
-let texte = "Bonjour"; // Variable texte = chaîne de caractère
+nombre = (2+3)*5;
 
-autreNombre = 15.36; // La variable autreNombre est de type : nombre
+// Modulo % renvoi le reste de la division
+nombre = nombre1 % nombre2;
+
+nombre = 15 % 4;
+
+// Incrément - décrément
+// Incrément de 4
+nombre = nombre + 4;
+nombre += 4;
+nombre += 1;
+
+// Incrément de 1
+nombre++;
+
+// Décrément de 3
+nombre = nombre - 3;
+nombre -= 3;
+
+// Décrément de 1
+nombre--;
+
+nombre *= 4;
+nombre /= 4;
+
 
 // Ecrire dans la console
-console.log("Brouette");    // Affiche "Brouette" dans la console
 console.log(nombre);        // Affiche le contenu de la variable nombre dans la console
-console.log(texte);         
 
