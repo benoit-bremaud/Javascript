b146f8d165fb8d3f5191a5e1222326f700ed9a55
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-13 13:34:36 +0200

    feat(script): Implement basic DOM manipulation examples
    This commit implements basic examples of DOM manipulation in the script.js file.
    
    The code includes the following functionalities:
    
    1. Querying and logging all elements with the "h2" tag using `document.querySelectorAll("h2")`.
    2. Accessing the second `li` element using the CSS selector `li:nth-child(2)` and logging it.
    3. Querying and logging all elements with the "row" class using `document.querySelectorAll(".row")`.
    4. Querying and logging an element with the "titre2" id using `document.querySelector("#titre2")`.
    5. Logging each element in the `classeRow` collection using a `for...of` loop.
    
    These code changes demonstrate the basics of manipulating the Document Object Model (DOM) in JavaScript.
    
    Note: The commit message assumes that you are following the Angular commit convention.
    
    References:
    - Angular Commit Message Guidelines: https://github.com/angular/angular/blob/master/CONTRIBUTING.md#commit
    - JavaScript DOM Manipulation: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model

--- js/script.js
+++ js/script.js
@@ -1,70 +1,23 @@
-// L'opérateur de décomposition
+// Manipuler le document (DOM) Document Object Model
 
-const nombres = [25, 12, 8];
+// Aller chercher un/des élément(s) dans le document
+// Aller chercher tous ce qui a la balise h2
+let baliseH2 = document.querySelectorAll("h2");
+console.log(baliseH2);
 
-function addition(nb1, nb2, nb3) {
-    return nb1 + nb2 + nb3;
+
+// Acceder au 2e li grace au selecteur CSS
+let baliseLi = document.querySelector("li:nth-child(2)");
+console.log(baliseLi);
+
+// Aller chercher un élément avec une classe
+let classeRow = document.querySelectorAll(".row");
+console.log(classeRow);
+
+// Aller chercher un élément avec un id
+let titre2 = document.querySelector("#titre2");
+console.log(titre2);
+
+for (let row of classeRow) {
+    console.log(row);
 }
-
-let resultat = addition(10, 20, 30);
-console.log(resultat);
-
-// Exemple 2
-resultat = addition(nombres[0], nombres[1], nombres[2]);
-console.log(resultat);
-
-// Exemple 3 
-resultat = addition(...nombres);
-console.log(resultat);
-
-// Exemple 2 et Exemple 3 donnent exéctement le même résultat
-
-console.log(nombres); // Affiche un array
-
-console.log(...nombres); // Eclate le tableau et affiche chaque valeur du tableau les unes derrières les autres
-
-// Récupération de la date du jour
-let maDate = new Date();
-// Affichage sur console
-console.log(maDate);
-
-// Initialisation d'une date
-maDate = new Date(2023, 7, 13);
-
-console.log(maDate);
-
-// Initialisation d'un tableau avec date
-const tableauDate = [2023, 7, 13];
-// Récupération d'une date dans un tableau
-maDate = new Date(...tableauDate);
-
-console.log(maDate);
-
-// Inclure un tableau à l'intérieur d'un autre
-const tableauNombres = [48, 74, ...nombres, 26];
-
-console.table(tableauNombres);
-
-const t1 = ["a", "b", "c"];
-const t2 = ["d", "e", "f"];
-
-// Concatenation de t1 et t2 et crée t3
-const t3 = [...t1, ...t2];
-
-console.table(t3);
-
-// Avec des objets
-// Déclaration d'un objet
-const nom = {
-    nom: "Bremaud"
-};
-
-// Autre objet
-const prenom = {
-    prenom: "Benoit"
-};
-
-// Nouvel Objet
-const profil = {...nom, ...prenom};
-
-console.log(profil);
