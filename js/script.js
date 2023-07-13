3b86c6a817d9471a599e90b1a2e872aac114acb9
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-13 11:42:35 +0200

    feat(script): Add array manipulation examples with for and while loops
    This commit adds code that demonstrates the usage of `for` and `while` loops for manipulating arrays. It includes examples of iterating through arrays using `for` loops and using `for...in`, `for...of`, and `forEach` loops. The code showcases different looping techniques and their respective use cases.

--- js/script.js
+++ js/script.js
@@ -1,160 +1,63 @@
-// Conditions et opérations de comparaison
+// Les boucles
 
-/* 
-Si le temps est beau
-    Je prends mes lunettes de soleil
-Sinon
-    Je prends mon parapluie
-*/
-
-let nombre = 12;
-
-// On pose une question
-if (nombre == 12) {
-    // La consition est remplie
-    console.log("Le nombre est égal à 12");
-}
-// Autre exemple
-nombre = 122;
-
-// On pose une question
-if (nombre == 12) {
-    // La condition est remplie
-    console.log("Le nombre est égal à 12");
-} else {
-    // La consition n'est pas rempli
-    console.log("Le nombre n'est pas égal à 12");
+// Boucle FOR
+for (let compteur = 0; compteur <= 10; compteur++) {
+    console.log(compteur);    
 }
 
-// Autre exemple
-nombre = "12";
-
-// On pose une question
-if (nombre == 12) {
-    // La condition est remplie
-    console.log("Le nombre est égal à 12");
-} else {
-    // La consition n'est pas rempli
-    console.log("Le nombre n'est pas égal à 12");
+for (let compteur = 0; compteur <= 10; compteur += 5) {
+    console.log(compteur);    
 }
 
-// `==` ne compare que la valeur (égalité faible) `===` compare la valeur ET le type (égalité forte)
-
-nombre = "12";
-
-// On pose une question
-if (nombre === 12) {
-    // La condition est remplie (valeur ET type)
-    console.log("Le nombre est égal à 12");
-} else {
-    // La consition n'est pas rempli (ici valeur OK, type NOK)
-    console.log("Le nombre n'est pas égal à 12");
+for (let compteur = 20; compteur >= 0; compteur--) {
+    console.log(compteur);    
 }
 
-nombre = 12;
-
-// != inégalité
-if (nombre != 12) {
-    // La condition est remplie (en valeur seulement)
-    console.log("Le nombre n'est pas égal à 12");
-} else {
-    // La consition n'est pas rempli (en valeur seulement)
-    console.log("Le nombre est égal à 12");
+for (let compteur = 100; compteur >= 0; compteur -= 10) {
+    console.log(compteur);    
 }
 
-nombre = "12";
+let pass = "azerty";
 
-// != inégalité faible
-if (nombre != 12) {
-    // La condition est remplie (en valeur seulement)
-    console.log("Le nombre n'est pas égal à 12");
-} else {
-    // La consition n'est pas rempli (en valeur seulement)
-    console.log("Le nombre est égal à 12");
+// Boucle WHILE
+
+// while (pass != "brouette") {
+//     pass = prompt("Entrez le mot de passe");
+// }
+
+let clients =["Robert", "Bruno", "Pascale", "Benoit"];
+console.table(clients);
+
+// Parcours le tableau avec une boucle FOR
+for (let ligne = 0; ligne < clients.length; ligne++) {
+    console.log(clients[ligne]);
 }
 
-nombre = "12";
-
-// !== inégalité stricte
-if (nombre !== 12) {
-    // La condition est remplie (en valeur et en type)
-    console.log("Le nombre n'est pas égal à 12");
-} else {
-    // La consition n'est pas rempli (valeur OK, type NOK)
-    console.log("Le nombre est égal à 12");
+// Parcours avec boucle FOR IN quand on a besoin de la ligne
+for (let ligne in clients) {
+    console.log(ligne);
 }
 
-nombre = 12;
-
-// < plus petit que
-if (nombre < 12) {
-    // La condition est remplie
-    console.log("Le nombre est plus petit que 12");
-} else {
-    // La consition n'est pas rempli
-    console.log("Le nombre n'est pas plus petit que 12");
+for (let ligne in clients) {
+    console.log(clients[ligne]);
 }
 
-nombre = 11;
-
-// <= plus petit ou égal à
-if (nombre <= 12) {
-    // La condition est remplie
-    console.log("Le nombre est plus petit ou égal à 12");
-} else {
-    // La consition n'est pas rempli
-    console.log("Le nombre n'est pas plus petit ou égal à 12");
+// Parcours le tableau avec boucle FOR OF quand on a besoin de la valeur
+for (let valeur of clients) {
+    console.log(valeur);
 }
 
-// <, <=, >, >=
+// Parcours le tableau avec une boucle FOREACH quand on a besoin de la ligne ET de la valeur
+// On retour la valeur
+clients.forEach((valeur) => {
+    console.log(valeur); // Retourne Valeur
+});
 
-// condition avec des booléens
-let bool = true;
+// On retour la valeur ET l'index
+clients.forEach((valeur, index) => {
+    console.log(valeur, index); // Retourne Valeur puis Index
+});
 
-if (bool === true) {
-    // La condition est remplie (valeur ET type)
-    console.log("Le booléen est à true");
-} else {
-    // La consition n'est pas rempli (ici valeur OK, type NOK)
-    console.log("Le booléen est à false");
-}
-
-// On peut aussi écrire de la façon suivante
-if (bool) {
-    // La condition est remplie (valeur ET type)
-    console.log("Le booléen est à true");
-} else {
-    // La consition n'est pas rempli (ici valeur OK, type NOK)
-    console.log("Le booléen est à false");
-}
-
-// On peut mixer
-if (bool && nombre < 12) {
-    // La condition est remplie (valeur ET type)
-    console.log("Le booléen est à true et le nombre est plus petit que 12");
-} else {
-    // La consition n'est pas rempli (ici valeur OK, type NOK)
-    console.log("Le booléen est à false");
-}
-
-// Autre exemple
-nombre = 13;
-bool = false;
-
-if (bool || nombre < 12) {
-    // La condition est remplie (valeur ET type)
-    console.log("Le booléen est à true OU le nombre est plus petit que 12");
-} else {
-    // La consition n'est pas rempli (ici valeur OK, type NOK)
-    console.log("Le booléen est à false et le nombre n'est pas plus petit que 12");
-}
-
-// Autre exemple
-if (bool || nombre < 12 && nombre > 5) {
-    // La condition est remplie (valeur ET type)
-    console.log("Le booléen est à true OU le nombre est plus petit que 12");
-} else {
-    // La consition n'est pas rempli (ici valeur OK, type NOK)
-    console.log("Le booléen est à false et le nombre n'est pas plus petit que 12");
-}
-
+clients.forEach((valeur, index, tableau) => {
+    console.log(tableau); // Retourne array
+});
