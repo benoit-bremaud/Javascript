9ad66fbdd922a7ab95395f7adb1c4fc7c0e1fc42
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-13 14:51:05 +0200

    feat(script): Implement DOM event listeners and element manipulation
    This commit implements DOM event listeners and element manipulation in the script.js file.
    
    The code demonstrates the following functionality:
    
    1. Waits for the DOMContentLoaded event before executing the code.
    2. Retrieves elements from the document using various selectors.
    3. Accesses the second li element using a CSS selector and logs it to the console.
    4. Retrieves elements with a specific class using querySelectorAll and applies a style change.
    5. Retrieves an element with a specific ID using querySelector and logs it to the console.
    6. Adds event listeners to h2 elements for click, mouseenter, and mouseleave events.
    7. Adds event listeners to the baliseLi element for click, mouseenter, and mouseleave events.
    8. Implements event handler functions for changing the color of elements, adding a border on mouseenter, and removing the border on mouseleave.
    
    This commit enhances the functionality of the code by adding event listeners and manipulating the elements based on the events triggered.
    
    Note: The commit message assumes that you are following the Angular commit convention. If you're using a different convention, you may modify the commit message accordingly.
    
    References:
    - Angular Commit Message Guidelines: https://github.com/angular/angular/blob/master/CONTRIBUTING.md#commit
    - DOM Event Listeners: https://developer.mozilla.org/en-US/docs/Web/API/EventListener
    - DOM Element Manipulation: https://developer.mozilla.org/en-US/docs/Web/API/Element

--- js/script.js
+++ js/script.js
@@ -1,23 +1,58 @@
-// Manipuler le document (DOM) Document Object Model
+// Manipuler le document (DOM) Document Object Model - Partie 2
+// On place un écouteur d'événement
+window.addEventListener("DOMContentLoaded", () => {
+    // Ici on est certains que le DOM est chargé
+    
+    // Aller chercher un/des élément(s) dans le document
+    // Aller chercher tous ce qui a la balise h2
+    let balisesH2 = document.querySelectorAll("h2");
+    // console.log(baliseH2);
+    
+    
+    // Acceder au 2e li grace au selecteur CSS
+    let baliseLi = document.querySelector("li:nth-child(2)");
+    console.log(baliseLi);
+    
+    // Aller chercher un élément avec une classe
+    let classeRow = document.querySelectorAll(".row"); // Attention, si on utilise SelectorAll, il faut par la suite boucler
+    console.log(classeRow);
+    
+    // Aller chercher un élément avec un id
+    // let titre2 = document.querySelector("#titre2");
+    // console.log(classeRow);
+    
+    for (let row of classeRow) {
+        row.style.color ="red";
+    }
 
-// Aller chercher un/des élément(s) dans le document
-// Aller chercher tous ce qui a la balise h2
-let baliseH2 = document.querySelectorAll("h2");
-console.log(baliseH2);
+    // On écoute le clic sur les balises h2
+    // console.log(baliseH2);
+    for (let baliseH2 of balisesH2) {        
+        baliseH2.addEventListener("click", changeCouleur);
+        baliseH2.addEventListener("mouseenter", ajoutBordrues);
+        baliseH2.addEventListener("mouseleave", retraitBordures);
+            // console.log(this);
+            // this.style.color = "green";
+    }
 
+    baliseLi.addEventListener("click", changeCouleur);
+    baliseLi.addEventListener("mouseenter", ajoutBordrues);
+    baliseLi.addEventListener("mouseleave", retraitBordures);
 
-// Acceder au 2e li grace au selecteur CSS
-let baliseLi = document.querySelector("li:nth-child(2)");
-console.log(baliseLi);
+});
 
-// Aller chercher un élément avec une classe
-let classeRow = document.querySelectorAll(".row");
-console.log(classeRow);
-
-// Aller chercher un élément avec un id
-let titre2 = document.querySelector("#titre2");
-console.log(titre2);
-
-for (let row of classeRow) {
-    console.log(row);
+function changeCouleur() {
+    this.style.color = "green";
 }
+
+function ajoutBordrues() {
+    this.style.border = "1px solid black";
+    this.removeEventListener("mouseenter", ajoutBordrues);
+}
+
+function retraitBordures() {
+    this.style.border = "none";
+    this.removeEventListener("mouseleave", retraitBordures);
+
+}
+
