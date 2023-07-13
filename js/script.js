01a2ea29756da0d24f650be4a1f4743458245a81
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-13 12:47:07 +0200

    feat(script): Implement spread operator examples for iterable objects
    This commit implements the usage of the spread operator to handle iterable objects in JavaScript. The code changes can be found in the script.js file.
    
    The implemented code demonstrates the following examples:
    
    1. Usage of the spread operator to pass individual elements as arguments to the "addition" function.
    2. Usage of the spread operator to pass array elements as arguments to the "addition" function.
    3. Usage of the spread operator to pass the elements of the "nombres" array directly as arguments to the "addition" function.
    4. Usage of the spread operator to separate and display each value of the "nombres" array.
    5. Usage of the spread operator to initialize a Date object with values from an array.
    6. Usage of the spread operator to include one array inside another.
    7. Usage of the spread operator to concatenate arrays.
    8. Usage of the spread operator with objects to merge their properties.
    
    These code changes enhance the understanding and usage of the spread operator in JavaScript.
    
    Note: The commit message assumes that you are following the Angular commit convention. If you're using a different convention, you may modify the commit message accordingly.
    
    References:
    - Angular Commit Message Guidelines: https://github.com/angular/angular/blob/master/CONTRIBUTING.md#commit
    - JavaScript Spread Syntax: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax

--- js/script.js
+++ js/script.js
@@ -1,49 +1,70 @@
-// Les fonctions
+// L'opérateur de décomposition
 
-// Fonction qui va dire "bonjour"
+const nombres = [25, 12, 8];
 
-// Déclaration et initialisation de la fonction direBonjour
-function direBonjour() {
-    console.log("Bonjour");
+function addition(nb1, nb2, nb3) {
+    return nb1 + nb2 + nb3;
 }
 
-// Appel de la fonction direBonjour
-direBonjour();
-
-console.log("Benoit");
-
-direBonjour();
-
-// Déclaration et initialisation d'une fonction avec paramètres, valeurs par défaut et son JSDoc
-/**
- * Cette fonction fait un console.log qui dit "Bonjour"
- * @param {string} prenom Prénom de la personne
- * @param {string} nom Nom de la personne
- */
-function direBonjoura(prenom = "Benoit", nom = "Bremaud") {
-    console.log(`Bonjour ${prenom} ${nom}`);
-}
-
-direBonjoura("Benoit", "Bremaud");
-direBonjoura("Viktor");
-direBonjoura("Roger", "Rabbit");
-
-direBonjoura("Benoit", "Bremaud");
-
-direBonjoura();
-
-// Fonction qui retourne une valeur
-/**
- * Cette fonction additionne 2 nombres
- * @param {number} nb1 Nombre 1
- * @param {number} nb2 Nombre 2
- * @returns number
- */
-function addition(nb1, nb2) {
-    let total = nb1 + nb2;
-    return total;
-}
-
-let resultat = addition(5, 12);
-
+let resultat = addition(10, 20, 30);
 console.log(resultat);
+
+// Exemple 2
+resultat = addition(nombres[0], nombres[1], nombres[2]);
+console.log(resultat);
+
+// Exemple 3 
+resultat = addition(...nombres);
+console.log(resultat);
+
+// Exemple 2 et Exemple 3 donnent exéctement le même résultat
+
+console.log(nombres); // Affiche un array
+
+console.log(...nombres); // Eclate le tableau et affiche chaque valeur du tableau les unes derrières les autres
+
+// Récupération de la date du jour
+let maDate = new Date();
+// Affichage sur console
+console.log(maDate);
+
+// Initialisation d'une date
+maDate = new Date(2023, 7, 13);
+
+console.log(maDate);
+
+// Initialisation d'un tableau avec date
+const tableauDate = [2023, 7, 13];
+// Récupération d'une date dans un tableau
+maDate = new Date(...tableauDate);
+
+console.log(maDate);
+
+// Inclure un tableau à l'intérieur d'un autre
+const tableauNombres = [48, 74, ...nombres, 26];
+
+console.table(tableauNombres);
+
+const t1 = ["a", "b", "c"];
+const t2 = ["d", "e", "f"];
+
+// Concatenation de t1 et t2 et crée t3
+const t3 = [...t1, ...t2];
+
+console.table(t3);
+
+// Avec des objets
+// Déclaration d'un objet
+const nom = {
+    nom: "Bremaud"
+};
+
+// Autre objet
+const prenom = {
+    prenom: "Benoit"
+};
+
+// Nouvel Objet
+const profil = {...nom, ...prenom};
+
+console.log(profil);
